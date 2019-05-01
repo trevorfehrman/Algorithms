ffc6baf950f0b6e7998d00798f39be44bd9b12a3
Trevor Fehrman <trevorfehrman@gmail.com>
2019-04-30 22:53:41 -0700

    recipe batches and stock prices

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,34 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    count = 0
+
+    while True:
+        for key, value in recipe.items():
+            if key in ingredients:
+                if ingredients[key] - recipe[key] < 0:
+                    print(count)
+                    return count
+                ingredients[key] -= recipe[key]
+            else:
+                return count
+        count += 1
+
+# 0, 1, 2, 100
 
 
-if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+recipe_batches({'milk': 100, 'flour': 4, 'sugar': 10, 'butter': 5}, {
+               'milk': 1288, 'flour': 9, 'sugar': 95})
+recipe_batches({'milk': 100, 'butter': 50, 'cheese': 10},
+               {'milk': 198, 'butter': 52, 'cheese': 10})
+recipe_batches({'milk': 2, 'sugar': 40, 'butter': 20}, {
+               'milk': 5, 'sugar': 120, 'butter': 500})
+recipe_batches({'milk': 2}, {'milk': 200})
+# iVjjjf __name__ == '__main__':
+#   # Change the entries of these dictionaries to test
+#   # your implementation with different inputs
+#   recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
+#   ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+#   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -1,15 +1,28 @@
 #!/usr/bin/python
-
+import math
 import argparse
 
+
 def find_max_profit(prices):
-  pass
+
+    leader = -math.inf
+
+    for i in range(len(prices) - 2):
+
+        for j in range(i + 1, len(prices) - 1):
+            difference = prices[j] - prices[i]
+            if difference > leader:
+                leader = difference
+    return leader
 
 
-if __name__ == '__main__':
-  # This is just some code to accept inputs from the command line
-  parser = argparse.ArgumentParser(description='Find max profit from prices.')
-  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-  args = parser.parse_args()
+find_max_profit([100, 90, 80, 50, 20, 10])
 
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+
+# if __name__ == '__main__':
+#   # This is just some code to accept inputs from the command line
+#   parser = argparse.ArgumentParser(description='Find max profit from prices.')
+#   parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
+#   args = parser.parse_args()
+
+#   print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
