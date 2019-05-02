d6c917c1f8207893553336384bed393aab755c6d
Trevor Fehrman <trevorfehrman@gmail.com>
2019-05-02 15:43:28 -0700

    mvp

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -1,12 +1,12 @@
 import sys
 
 
-# def eating_cookies(n, cache=[1, 1, 2]):
-#     if not n in range(0, len(cache)):
-#         value = eating_cookies(
-#             n - 1) + eating_cookies(n - 2) + eating_cookies(n - 3)
-#         cache.append(value)
-#     return cache[n]
+def eating_cookies(n, cache=[1, 1, 2]):
+    if not n in range(0, len(cache)):
+        value = eating_cookies(
+            n - 1) + eating_cookies(n - 2) + eating_cookies(n - 3)
+        cache.append(value)
+    return cache[n]
 
 
 
