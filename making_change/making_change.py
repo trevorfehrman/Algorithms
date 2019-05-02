42e98ff8340db6f45f8d562598e643cb90e3358a
Trevor Fehrman <trevorfehrman@gmail.com>
2019-05-02 15:43:14 -0700

    mvp

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -2,16 +2,15 @@
 
 import sys
 
-def making_change(amount, denominations):
-  pass 
 
+def making_change(amount, denominations, cache=None):
+    if not cache:
+        cache = {int: 0 for int in range(amount + 1)}
+        cache[0] = 1
 
-if __name__ == "__main__":
-  # Test our your implementation from the command line
-  # with `python making_change.py [amount]` with different amounts
-  if len(sys.argv) > 1:
-    denominations = [1, 5, 10, 25, 50]
-    amount = int(sys.argv[1])
-    print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
-  else:
-    print("Usage: making_change.py [amount]")
+    for coin in denominations:
+        for higher_amount in range(coin, amount + 1):
+            difference = higher_amount - coin
+            cache[higher_amount] += cache[difference]
+
+    return cache[amount]
